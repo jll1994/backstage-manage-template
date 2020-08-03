b32483d9b1200f58eb5c669c663c172e68d91cf2
jianglulu <2845957073@qq.com>
2020-08-03 17:12:17 +0800

    初始化版本库，配置less全局变量，配置路径别名，生产环境开启gizp压缩和去除console.log“

---
+++ vue.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  devServer: {
+    open: true,
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("@assets", resolve("src/assets"))
+      .set("@views", resolve("src/views"))
+      .set("@components", resolve("src/components"));
+  },
+  configureWebpack: (config) => {
+    const plugins = [];
+    if (isProd) {
+      // 配置webpack 压缩
+      plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css$/,
+          threshold: 4096, // 超过4kb压缩
+        })
+      );
+    }
+    config.plugins = [...config.plugins, ...plugins];
+  },
+  css: {
+    loaderOptions: {
+      // 向 CSS 相关的 loader 传递选项
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.join(__dirname, "./src/assets/styles/variable.less")],
+    },
+  },
+};
